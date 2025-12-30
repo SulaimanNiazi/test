0358dc156f82f0fb2f116980b727015c31b3d4be
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-12-30 16:59:38 +0500

    test3

--- main/log.h
+++ main/log.h
@@ -2,6 +2,7 @@
 
 #include "esp_log.h"
 #include "esp_err.h"
+#include <stdbool.h>
 
 void init_log(size_t count, ...);
 bool loge_success(const char *tag, const esp_err_t error, const char *detail);
